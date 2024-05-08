b4caa7382e90e2fe4daa61bc32bd520749bfe57e
Breno Kleine Bernstein <87719398+brenokleine@users.noreply.github.com>
2024-05-07 22:01:14 -0300

    this is a bad commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
 
-import { Skeleton, SkeletonCircle, SkeletonText } from '@chakra-ui/react'
-import { Stack } from '@chakra-ui/react'
+import Feed from "@/components/Feed"
 
-const page = () => {
+const Home = () => {
   
   
     return (
-    <div>
+        <section className=' w-full flex flex-col items-center justify-center'>
+            {/* svg logo here */}
 
-    </div>
+
+            <h1 className=" text-6xl text-center text-primary font-bold poetsen-font flex flex-col gap-3">
+                Discover & share your quotes!
+                
+                <span className="text-3xl text-tertiary">
+                    The best quotes from the internet are here...
+                </span>
+            </h1>
+            <p className=" w-1/2 text-center text-xl text-primary pt-12">
+                Welcome to our quote-sharing mini blog! Discover inspiration in every post and join our community in spreading positivity through the power of words.
+            </p>
+
+            <Feed 
+                
+            />
+
+        </section>
   )
 }
 
-export default page
+export default Home
