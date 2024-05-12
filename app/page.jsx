fe7504c0ef7d252f49cf5cd63a4265bf181fdb74
Breno Kleine Bernstein <87719398+brenokleine@users.noreply.github.com>
2024-05-12 12:13:23 -0300

    reorganizing main page + chakraui icons

--- app/page.jsx
+++ app/page.jsx
@@ -7,20 +7,24 @@ const Home = () => {
     return (
         <section className=' w-full flex flex-col items-center justify-center'>
 
-            <h1 className=" text-6xl text-center text-primary font-bold poetsen-font flex flex-col gap-3">
-                Discover & share your quotes!
+            <div className="flex flex-col justify-center items-center gap-6">
+                <h1 className=" text-6xl text-center text-primary font-bold poetsen-font flex flex-col gap-3">
+                    Discover & share your quotes!
                 
-                <span className="text-3xl text-tertiary">
-                    The best quotes from the internet are here...
-                </span>
-            </h1>
-            <p className=" w-1/2 text-center text-xl text-primary pt-12">
-                Welcome to our quote-sharing mini blog! Discover inspiration in every post and join our community in spreading positivity through the power of words.
-            </p>
+                    <span className="text-3xl text-tertiary">
+                        The best quotes from the internet are here...
+                    </span>
+                </h1>
+                <p className=" w-1/2 text-center text-xl text-primary">
+                    Welcome to our quote-sharing mini blog! Discover inspiration in every post and join our community in spreading positivity through the power of words.
+                </p>
+            </div>
 
-            <Feed 
+            <div className="w-full p-4 flex justify-center mt-10">
+                <Feed
                 
-            />
+                />
+            </div>
 
         </section>
   )
