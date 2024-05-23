c4e407d5f86d90f9b7995c7a0dabb9da0163b3de
Breno Kleine Bernstein <87719398+brenokleine@users.noreply.github.com>
2024-05-23 17:50:09 -0300

    responsive nav working + small feed change

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,18 +5,30 @@ import Image from 'next/image'
 import { useState, useEffect } from 'react'
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
-import { Avatar, Button } from '@chakra-ui/react'
+import { Avatar, Button, useDisclosure } from '@chakra-ui/react'
+
+import {
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from '@chakra-ui/react'
+import { HamburgerIcon, SmallAddIcon } from '@chakra-ui/icons'
+
+import { LogOut } from '@blueprintjs/icons'
  
 const Nav = () => {
 
   const { data: session } = useSession();
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   const [providers, setProviders] = useState(null)
 
   useEffect(() => {
     const setUpProviders = async () => {
       const res = await getProviders()
-
       setProviders(res)
     }
 
@@ -25,7 +37,7 @@ const Nav = () => {
   
   return (
     <nav className="bg-secondary p-5 text-white w-full">
-      <div className="mx-auto flex flex-col flex-wrap items-center md:flex-row md:justify-between">
+      <div className="mx-auto flex justify-between">
         <div className='mx auto flex justify-start items-center gap-5'>
           <div>
             <Link href='/quotes' className="text-white flex justify-center items-center gap-2">
@@ -33,53 +45,147 @@ const Nav = () => {
               <p className='text-lg poetsen-font'>Tweetopia</p>
             </Link>
           </div>
-          <ul className="flex gap-2">
+          <ul className="flex gap-2 hidden md:block">
             <Link className="transition ease-in delay-100 hover:bg-primary p-2 rounded-full" href="/">Home</Link>
             <Link className="transition ease-in delay-100 hover:bg-primary p-2 rounded-full" href="/quotes">Quotes</Link>
             <Link className="transition ease-in delay-100 hover:bg-primary p-2 rounded-full" href="/contact">Contact</Link>
           </ul>
         </div>
         
-        {session?.user ? (
-          <>
-            <div className='flex gap-3 md:gap-5 items-center justify-center'>
-              <Link href="/create-quote" className="flex justify-center items-center bg-quaternary text-primary rounded-full p-2 text-md poetsen-font">
-                Create Quote
-              </Link>
-
-              <Button 
-                onClick={signOut}
-                colorScheme={'blackAlpha'}
-              >
-                Sign Out
-              </Button>
-
-              <Link
-                href="/profile"
-                className="flex justify-center items-center p-1"
-              >
-                <Avatar size={'md'} src={session?.user.image} className='rounded-full' alt="user" />
-              </Link>
-            </div>
-
-          </>
-        ) : (
-          <>
-              <div className='flex gap-3 md:gap-5 items-center justify-center pr-8'>
-                {providers && 
-                  Object.values(providers).map((provider) => (
-                    <Button 
-                      key={provider.name}
-                      onClick={() => signIn(provider.id)}
-                      colorScheme={'blackAlpha'}
-                    >
-                      Sign in with {provider.name}
-                    </Button>
-                  ))}
+        <div className='hidden md:block'>
+          {session?.user ? (
+            <>
+              <div className='flex gap-3 md:gap-5 items-center justify-center'>
+                <Link href="/create-quote" className="flex justify-center items-center bg-quaternary text-primary rounded-full p-2 text-md poetsen-font">
+                  Create Quote
+                </Link>
+                <Button
+                  onClick={signOut}
+                  colorScheme={'blackAlpha'}
+                >
+                  Sign Out
+                </Button>
+                <Link
+                  href="/profile"
+                  className="flex justify-center items-center p-1"
+                >
+                  <Avatar size={'md'} src={session?.user.image} className='rounded-full' alt="user" />
+                </Link>
               </div>
-          </>
-        )}
-        
+            </>
+          ) : (
+            <>
+                <div className='flex gap-3 md:gap-5 items-center justify-center pr-8'>
+                  {providers &&
+                    Object.values(providers).map((provider) => (
+                      <Button
+                        key={provider.name}
+                        onClick={() => signIn(provider.id)}
+                        colorScheme={'blackAlpha'}
+                      >
+                        Sign in with {provider.name}
+                      </Button>
+                    ))}
+                </div>
+            </>
+          )}
+        </div>
+
+        <div className='block md:hidden flex justify-center items-center'>
+          <Button bg={'tertiary'} rounded={'full'} size={'md'} onClick={onOpen} >
+            <HamburgerIcon color={'white'}/>
+          </Button>
+          <Drawer
+            isOpen={isOpen}
+            placement='right'
+            onClose={onClose}
+          >
+            <DrawerOverlay/>
+            <DrawerContent
+              roundedLeft={'xl'}
+              rounded
+            >
+              <DrawerCloseButton 
+                color='white'
+              />
+              <DrawerHeader
+                bg='tertiary'
+                color='white'
+                roundedTopLeft={'xl'}
+              >
+                Menu
+              </DrawerHeader>
+
+              <DrawerBody
+                color='primary'
+                sx={{
+                  backgroundImage: 'url("/assets/images/bg4.svg")',
+                  backgroundSize: 'cover',
+                  backgroundRepeat: 'no-repeat',
+                  backgroundPosition: 'start',
+                }}
+              >
+                <div>
+                  {session?.user ? (
+                    <div className='flex items-center justify-between mb-1 mt-1'>
+                      <Link
+                        href="/profile"
+                        className="flex justify-center items-center p-3 pl-1"
+                        onClick={onClose}
+                      >
+                        <Avatar size={'md'} src={session?.user.image} className='rounded-full' alt="user" />
+                        {/* @username here after context is implemented */}
+                      </Link>
+                      <Button
+                        onClick={signOut}
+                        rounded={'full'}
+                        p={2}
+                        display={'flex'}
+                        alignItems={'center'}
+                        justifyContent={'center'}
+                      >
+                        <LogOut 
+                          color={'red'}
+                        />
+                      </Button>
+                    </div>
+                  ) : (
+                    <div className='flex items-center justify-center mb-4 mt-4'>
+                      {providers &&
+                        Object.values(providers).map((provider) => (
+                          <Button
+                            key={provider.name}
+                            onClick={() => signIn(provider.id)}
+                            bg={'tertiary'}
+                            _hover={{ bg: 'primary' }}
+                            color={'white'}
+                            className='hover:text-white transition ease-in'
+                          >
+                            Sign in with {provider.name}
+                          </Button>
+                        ))}
+                    </div>
+                  )}
+                </div>
+                <ul className="flex flex-col w-full mb-4 rounded-md" onClick={onClose}>
+                  <Link className="transition ease-in rounded-md hover:bg-tertiary hover:text-white p-3 w-full" href="/">Home</Link>
+                  <Link className="transition ease-in rounded-md hover:bg-tertiary hover:text-white p-3 w-full" href="/quotes">Quotes</Link>
+                  <Link className="transition ease-in rounded-md hover:bg-tertiary hover:text-white p-3 w-full" href="/contact">Contact</Link>
+                </ul>
+                  {session?.user && (
+                    <Link 
+                      href="/create-quote" 
+                      className="flex w-fit gap-1 justify-center items-center bg-quaternary text-primary hover:bg-primary hover:text-quaternary transition ease-in rounded-full p-2 text-md poetsen-font"
+                      onClick={onClose}
+                    >
+                      <SmallAddIcon />
+                      Create Quote
+                    </Link>
+                  )}
+              </DrawerBody>
+            </DrawerContent>
+          </Drawer>
+        </div>
       </div>
     </nav>
   )
