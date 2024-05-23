c2f50004b4518c7ec6fd3ddb47e99339b2feff08
Breno Kleine Bernstein <87719398+brenokleine@users.noreply.github.com>
2024-05-23 10:43:06 -0300

    posts on quote page now

---
+++ app/quotes/page.jsx
@@ -0,0 +1,12 @@
+import Feed from '@/components/Feed'
+import React from 'react'
+
+const Quotes = () => {
+  return (
+    <div className='w-full flex justify-center'>
+        <Feed/>
+    </div>
+  )
+}
+
+export default Quotes
