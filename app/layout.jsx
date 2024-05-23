a0b2e3bc49fb3fdce1afaad1c74ea5ad6839693d
Breno Kleine Bernstein <87719398+brenokleine@users.noreply.github.com>
2024-05-23 11:22:32 -0300

    looking good but body and html height problem

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,18 +16,18 @@ import Footer from '@/components/Footer'
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className='inter-font min-h-full h-screen flex flex-col flex-1 bg-cover bg-center bg-no-repeat bg-fixed ' style={{ backgroundImage: 'url(/assets/images/bg1.svg)' }} >
-          <CProvider>
-            <Provider> 
-              <div className='mb-16'>
-                <Nav />
-              </div>
-                {children}
-              <div className='mt-auto'>
-                <Footer/>
-              </div>
-            </Provider>
-          </CProvider>
+      <body className='min-h-full h-screen inter-font flex flex-col bg-cover bg-center bg-no-repeat bg-fixed backdrop-blur-sm ' style={{ backgroundImage: 'url(/assets/images/bg1.svg)' }} >
+        <CProvider>
+          <Provider> 
+            <div className='mb-16'>
+              <Nav />
+            </div>
+              {children}
+            <div className='mt-auto'>
+              <Footer/>
+            </div>
+          </Provider>
+        </CProvider>
       </body>
     </html>
   )
