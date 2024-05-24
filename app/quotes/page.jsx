1a02c56b8ceddc47004c3069a8f59808d1f13b83
Breno Kleine Bernstein <87719398+brenokleine@users.noreply.github.com>
2024-05-24 11:28:43 -0300

    one more try...

--- app/quotes/page.jsx
+++ app/quotes/page.jsx
@@ -1,24 +1,10 @@
-'use server'
-
-import Feed from '@/components/Feed'
+import FeedServer from '@/components/FeedServer'
 import React from 'react'
 
-const fetchPosts = async () => {
-  const res = await fetch('/api/quote');
-  const data = await res.json();
-
-  return data;
-}
-
 const Quotes = () => {
-
-  const initialPosts = async () => await fetchPosts();
-
   return (
     <div className='w-full flex justify-center'>
-        <Feed
-          posts={initialPosts}
-        />
+        <FeedServer/>
     </div>
   )
 }
---
+++ components/FeedServer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Feed from './Feed'
+
+const fetchPosts = async () => {
+    const res = await fetch('/api/quote');
+    const data = await res.json();
+    return data;
+}
+
+const FeedServer = async () => {
+    const initialPosts = await fetchPosts()
+
+    return (
+        <div className='w-full flex justify-center'>
+            <Feed posts={initialPosts} />
+        </div>
+    )
+}
+
+export default FeedServer
