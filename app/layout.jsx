253003eee191c7fef34c47368e6cf18a78b399a3
Breno Kleine Bernstein <87719398+brenokleine@users.noreply.github.com>
2024-06-11 18:12:11 -0300

    added icon

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,6 +16,9 @@ import Footer from '@/components/Footer'
 export default function RootLayout({ children }) {
   return (
     <html lang='en' className='h-full h-screen'>
+      <head>
+        <link rel="icon" href="/assets/icons/tweetopia_icon.png" />
+      </head>
       <body style={{ backgroundImage: 'url(/assets/images/bg1.svg)' }} className='min-h-full h-screen h-full inter-font flex flex-col bg-cover bg-center bg-no-repeat bg-fixed' >
         <CProvider>
           <Provider> 
